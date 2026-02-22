db1005ca06f69e582973520e4ae4b4701fd78236
Raúl Ferreyra <raulrferreyrav@gmail.com>
2026-02-22 01:08:18 -0500

    Fix images

--- kiubmedia/urls.py
+++ kiubmedia/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Personalización del sitio admin
 admin.site.site_header = "KIUB Media Administración"
