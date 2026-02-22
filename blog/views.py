4d01c09c79d474c0da311e7db6c19bbedce93326
Raúl Ferreyra <raulrferreyrav@gmail.com>
2026-02-21 22:46:23 -0500

    Added View and custom admin

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-pub_date')
+    latest_post = posts.first() if posts.exists() else None
+    other_posts = posts[1:] if latest_post else posts
+
+    return render(request, 'blog/post_list.html', {
+        'posts': other_posts,
+        'latest_post': latest_post,
+    })
